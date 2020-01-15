81cbbd738156857d2c6350f5ac4a6b43ffd215c7
LordeUnicorn <milpontia.ngwenya@umuzi.org>
2020-01-15 11:43:44 +0200

    Updated function to return True/False instead of raising exception

--- password_cheker.py
+++ password_cheker.py
@@ -35,24 +35,24 @@ password_is_valid('pasR3#sword')
 
 #========================================Password_is_okay==================================================#
 
-
 def password_is_ok(password):
     while True:
-        if (len(password)<8):
-                raise Exception('Password must be greater than 8')
-                #break
+        if (len(password)<9):
+            return False
         if not re.search("[a-z]", password):
-                raise Exception('password should have at least one lowercase letter')
-                #break
+            return False 
         elif not re.search("[A-Z]", password):
-                raise Exception('password should have at least one uppercase letter')
-                #break
-        else:  
-                print('Password is okay')
-                break
-             
-                 
-password_is_ok('dcxG6bxbfg')
+            return False
+            
+        else:
+            return True
+                
+            break
+                
+
+                
+                
+password_is_ok("rQ@DFGG1h")
             
 
 
